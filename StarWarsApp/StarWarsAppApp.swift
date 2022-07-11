632299d6333304cf27a5cbd3e797eb7f520a378d
Andrey I <iaa@bork.ru>
2022-07-11 17:50:54 +0200

    Initial Commit

---
+++ StarWarsApp/StarWarsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsAppApp.swift
+//  StarWarsApp
+//
+//  Created by atme on 11/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
